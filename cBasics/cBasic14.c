c63be165660307a6f2a3fd70a4fadd02b0309dab
dsnim <57756815+dsnim@users.noreply.github.com>
2020-09-17 16:22:44 +0900

    배열
    배열 선언
    배열의 요소 사용
    배열의 크기 계산
    정수 배열의 전체 데이터의 합
    특정 범위의 데이터 수
    배열안에서 가장 큰값 찾기
    2차원 배열

---
+++ cBasics/cBasic14.c
@@ -0,0 +1,21 @@
+﻿#include<stdio.h>
+
+/*
+   정수 배열의 전체 데이터의 합
+*/
+
+int main() {
+
+	int data[] = { 1, 5, 2, 8, 4 };
+
+	int dataSize = sizeof(data) / sizeof(int);
+
+	int total = 0;
+	for (int i = 0; i < dataSize; i++) {
+		printf("%d ", data[i]);
+		total += data[i];
+	}
+	puts("");
+	printf("데이터의 합 : %d\n", total);
+
+}
---
+++ cBasics/cBasic15.c
@@ -0,0 +1,27 @@
+﻿#include<stdio.h>
+
+/*
+   특정 범위의 데이터 수
+*/
+
+int main() {
+
+	int val[] = { 5, 2, 9, 4, 3, 12, 17 };
+
+	int valSize = sizeof(val) / sizeof(int);
+
+	int count = 0;
+	int startRange = 5;
+	int endRange = 15;
+	for (int i = 0; i < valSize; i++) {
+		printf("%d ", val[i]);
+	}
+	puts("");
+
+	for (int i = 0; i < valSize; i++) {
+		if (val[i] >= startRange && val[i] <= endRange)
+			++count;
+	}
+	printf("%d ~ %d 사이의 수 : %d\n", startRange, endRange, count);
+
+}
