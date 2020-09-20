bdd5e63096d9e262a6509617b269397fbd13a176
dsnim <57756815+dsnim@users.noreply.github.com>
2020-09-20 11:17:07 +0900

    함수
    함수
    함수 호출
    함수 정의
    재귀 호출

---
+++ cBasics/cBasic24.c
@@ -0,0 +1,43 @@
+﻿#include <stdio.h>
+
+int InputInt();
+void ShowAdd(int arg1, int arg2);
+int LastSum(int last);
+void ShowSum(int last, int sum);
+
+int main() {
+
+	int a;
+	//printf("숫자 입력 > "); scanf_s("%d", &a);
+	//printf("a : %d", a);
+	a = InputInt();
+	int b = InputInt();
+	ShowAdd(a, b);
+
+	int last = InputInt();
+	int sum = LastSum(last);
+	ShowSum(last, sum);
+}
+
+void ShowSum(int last, int sum) {
+	printf("1 ~ %d 까지의 합 : %d\n", last, sum);
+}
+
+int LastSum(int last) {
+	//int last = 10;
+	int sum = 0;
+	for (int i = 1; i <= last; i++) {
+		sum += i;
+	}
+	return sum;
+}
+
+void ShowAdd(int arg1, int arg2) {
+	printf("%d + %d = %d\n", arg1, arg2, arg1 + arg2);
+}
+
+int InputInt() {
+	int value;
+	printf("숫자 입력 > "); scanf_s("%d", &value);
+	return value;
+}
---
+++ cBasics/cBasic25.c
@@ -0,0 +1,16 @@
+﻿#include<stdio.h>
+
+// 재귀호출 : 프로그램 종료시 지나온순서대로 종료
+
+void RecursiveCall(int arg) {
+	printf("%d\n", arg);
+	if (arg < 3)
+		RecursiveCall(arg + 1);
+	printf("%d\n", arg);
+}
+
+int main() {
+
+	RecursiveCall(1);
+
+}
